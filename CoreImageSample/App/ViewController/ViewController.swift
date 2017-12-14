7393dc4bd714a018a6256946cb8b753123d1efb9
ha1f <ha1f@linecorp.com>
2017-12-14 12:31:30 +0900

    implement 塗りつぶし

--- CoreImageSample/App/ViewController/ViewController.swift
+++ CoreImageSample/App/ViewController/ViewController.swift
@@ -7,6 +7,12 @@
 //
 
 import UIKit
+import EasyImagy
+
+struct PixelPoint {
+    let x: Int
+    let y: Int
+}
 
 class ViewController: UIViewController {
     
@@ -28,7 +34,94 @@ class ViewController: UIViewController {
             imageView.heightAnchor.constraint(equalToConstant: 256)
             ])
         
-        imageView.image = QrCodeGenerator().generateUiImage(fromString: "https://ha1f.net", imageWidth: 256)
+        var image = Image<RGBA<UInt8>>(uiImage: #imageLiteral(resourceName: "sample.PNG"))
+        
+        
+        
+        // 塗りつぶし
+        let fillColor = RGBA<UInt8>.blue
+        var seedStack = [PixelPoint(x: 350, y: 220)]
+        if let startPointPixel = image.pixelAt(x: 350, y: 220) {
+            while true {
+                if let seed = seedStack.popLast() {
+                    // TODO: 許容範囲を修正する
+                    let isRegardedAsInside: (RGBA<UInt8>) -> Bool = { pixel in
+                        return pixel.red == startPointPixel.red
+                            && pixel.green == startPointPixel.green
+                            && pixel.blue == startPointPixel.blue
+                            && pixel.alpha == startPointPixel.alpha
+                    }
+                    
+                    // 右
+                    let rightEdgeX: Int = {
+                        for x in (seed.x+1)..<image.width {
+                            if !isRegardedAsInside(image[x, seed.y]) {
+                                return x-1
+                            }
+                            // 塗る
+                            image[x, seed.y] = fillColor
+                        }
+                        return image.width - 1
+                    }()
+                    // 左
+                    let leftEdgeX: Int = {
+                        for x in (0...seed.x).reversed() {
+                            if !isRegardedAsInside(image[x, seed.y]) {
+                                return x+1
+                            }
+                            // 塗る
+                            image[x, seed.y] = fillColor
+                        }
+                        return 0
+                    }()
+                    
+                    // 一行上
+                    let upperY = seed.y - 1
+                    if upperY >= 0 {
+                        var isInside = false
+                        for x in leftEdgeX...rightEdgeX {
+                            if !isRegardedAsInside(image[x, upperY]) {
+                                if isInside {
+                                    seedStack.append(PixelPoint(x: x-1, y: upperY))
+                                }
+                                isInside = false
+                            } else {
+                                isInside = true
+                            }
+                        }
+                        if isInside {
+                            seedStack.append(PixelPoint(x: rightEdgeX, y: upperY))
+                        }
+                    }
+                    
+                    // 一行下
+                    let lowerY = seed.y + 1
+                    if lowerY < image.width {
+                        var isInside = false
+                        for x in leftEdgeX...rightEdgeX {
+                            if !isRegardedAsInside(image[x, lowerY]) {
+                                if isInside {
+                                    seedStack.append(PixelPoint(x: x-1, y: lowerY))
+                                }
+                                isInside = false
+                            } else {
+                                isInside = true
+                            }
+                        }
+                        if isInside {
+                            seedStack.append(PixelPoint(x: rightEdgeX, y: lowerY))
+                        }
+                    }
+                } else {
+                    break
+                }
+            }
+        }
+        
+        imageView.image = image.uiImage
+        
+//        imageView.image = QrCodeGenerator().generateUiImage(fromString: "https://ha1f.net", imageWidth: 256)
+        
     }
 }
 
